c85e11df2f20531f67101a35fad349bb245b090d
Dalla Valle Horacio Daniel Jesus <dallavalle.h@pm.me>
2023-05-22 16:51:22 -0300

    Start creating tests for the main module

---
+++ tests/main_test.py
@@ -0,0 +1,17 @@
+"""
+Funciones que no voy a testear:
+- choose_source_folder: no posee lógica condicional.
+- choose_destination_folder: no posee lógica condicional.
+- inform_copy_completion: no posee lógica condicional.
+- inform_copy_cancellation: no posee lógica condicional.
+
+
+Funciones a testear:
+- get_folders_without_invoice
+- main?
+"""
+
+from source import main
+
+def test_get_folders_without_invoice():
+    pass
